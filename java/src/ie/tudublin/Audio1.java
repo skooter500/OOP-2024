b0f61274935366692bfcf0cd275aea28d7d83e40
skooter500 <bryan.duggan@tudublin.ie>
2024-02-28 13:37:07 +0000

    bla

---
+++ java/src/ie/tudublin/Audio1.java
@@ -0,0 +1,122 @@
+package ie.tudublin;
+
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import processing.core.PApplet;
+
+public class Audio1 extends PApplet
+{
+    Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
+    int mode = 0;
+
+    float[] lerpedBuffer;
+    float y = 0;
+    float smoothedY = 0;
+    float smoothedAmplitude = 0;
+
+    public void keyPressed() {
+		if (key >= '0' && key <= '9') {
+			mode = key - '0';
+		}
+		if (keyCode == ' ') {
+            if (ap.isPlaying()) {
+                ap.pause();
+            } else {
+                ap.rewind();
+                ap.play();
+            }
+        }
+	}
+
+    public void settings()
+    {
+        //size(1024, 1000, P3D);
+        fullScreen(P3D, SPAN);
+    }
+
+    public void setup()
+    {
+        minim = new Minim(this);
+        // Uncomment this to use the microphone
+        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        // ab = ai.mix; 
+        ap = minim.loadFile("heroplanet.mp3", 1024);
+        ap.play();
+        ab = ap.mix;
+        colorMode(HSB);
+
+        y = height / 2;
+        smoothedY = y;
+
+        lerpedBuffer = new float[width];
+    }
+
+    float off = 0;
+
+    public void draw()
+    {
+        //background(0);
+        float halfH = height / 2;
+        float average = 0;
+        float sum = 0;
+        off += 1;
+        // Calculate sum and average of the samples
+        // Also lerp each element of buffer;
+        for(int i = 0 ; i < ab.size() ; i ++)
+        {
+            sum += abs(ab.get(i));
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
+        }
+        average= sum / (float) ab.size();
+
+        smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
+        
+        float cx = width / 2;
+        float cy = height / 2;
+
+        switch (mode) {
+			case 0:
+                background(0);
+                // Code goes here
+                break;
+        case 1:
+            background(0);
+        
+            break;
+        case 2:
+            {
+                    // Code goes here
+            }
+        case 3:
+            // Code goes here
+        case 4:
+        
+            
+            break;
+
+        }
+        
+
+
+        
+        // Other examples we made in the class
+        /*
+        stroke(255);
+        fill(100, 255, 255);        
+        
+        circle(width / 2, halfH, lerpedA * 100);
+
+        circle(100, y, 50);
+        y += random(-10, 10);
+        smoothedY = lerp(smoothedY, y, 0.1f);        
+        circle(200, smoothedY, 50);
+        */
+
+    }        
+}
